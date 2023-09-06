a24d8b26fa8136c50a0c58e808781f15416ee4e4
Ivan <arias.ivan@gmail.com>
2023-09-06 01:16:40 -0400

    public folder updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,7 +12,7 @@ export default function About() {
                         <h5 className="card-title text-center">Ivan Arias</h5>
                         <h6 className="card-title text-center">Ivan Arias| Full Stack Development with Flask and React | Geologist |</h6>
                         <p className="card-text">I am learning to build front-ends with ReactJS. Also, I am gaining a thorough understanding of Flask to create JSON APIs and then combine them with a scalable React front end, resulting in slick, functional, and interactive full-stack applications. I have a background in geological modeling and mapping software (GIS). I want to apply all my experience and the tech skill set I am building at Flatiron School to help develop modern web applications.</p>
-                        <a href="/src/assets/IvanResume.pdf" download className="btn btn-primary">Download CV</a> {/* Adjust the href to the path of your PDF */}
+                        <a href="/assets/IvanResume.pdf" download className="btn btn-primary">Download CV</a>
                         <p className="card-text mt-3"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
