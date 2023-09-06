2ccbe2a60a0594ae4f07aadcea2bef1e062e2145
Ivan <arias.ivan@gmail.com>
2023-09-06 01:06:59 -0400

    cv added

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 export default function About() {
     return (
-
-        <div class="card mb-3">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="https://avatars.githubusercontent.com/u/52940441?v=4" class="img-fluid rounded-start" alt="..."/>
+        <div className="card mb-3">
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src="https://avatars.githubusercontent.com/u/52940441?v=4" className="img-fluid rounded-start" alt="Profile"/>
                 </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Ivan Arias</h5>
-                        <h6 class="card-title text-center">Ivan Arias| Full Stack Development with Flask and React | Geologist |</h6>
-                        <p class="card-text">I am learning to build front-ends with ReactJS. Also, I am gaining a thorough understanding of Flask to create JSON APIs and then combine them with a scalable React front end, resulting in slick, functional, and interactive full-stack applications. I have a background in geological modeling and mapping software (GIS). I want to apply all my experience and the tech skill set I am building at Flatiron School to help develop modern web applications.</p>
-                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title text-center">Ivan Arias</h5>
+                        <h6 className="card-title text-center">Ivan Arias| Full Stack Development with Flask and React | Geologist |</h6>
+                        <p className="card-text">I am learning to build front-ends with ReactJS. Also, I am gaining a thorough understanding of Flask to create JSON APIs and then combine them with a scalable React front end, resulting in slick, functional, and interactive full-stack applications. I have a background in geological modeling and mapping software (GIS). I want to apply all my experience and the tech skill set I am building at Flatiron School to help develop modern web applications.</p>
+                        <a href="/src/assets/Ivan_ Resume.pdf" download className="btn btn-primary">Download CV</a> {/* Adjust the href to the path of your PDF */}
+                        <p className="card-text mt-3"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
             </div>
