eee3c687b9daf44ea04744361c559600a45d2867
Ivan <arias.ivan@gmail.com>
2023-09-14 10:46:39 -0400

    footer updated

--- src/components/FooterMdb.js
+++ src/components/FooterMdb.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
 
 export default function App() {
   return (
@@ -7,24 +8,20 @@ export default function App() {
 
 
     <div class="d-flex justify-content-around bg-secondary-subtle ">
+
+      <div class=" p-3">
+        <a className="social-icon" href="https://github.com/hcoco1" aria-label='github' target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github fa-2xl" style={{ color: 'black' }}></i>
+        </a>
+      </div>
       <div class="p-3">
         <a className="social-icon" href="https://www.linkedin.com/in/arias-ivan-hcoco1/" aria-label='linkedin' target="_blank" rel="noopener noreferrer">
           <i className="fab fa-linkedin-in fa-2xl"></i>
         </a>
       </div>
       <div class=" p-3">
-        <a className="social-icon" href="https://github.com/hcoco1" aria-label='github' target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-github fa-2xl" style={{ color: 'black' }}></i>
-        </a>
-      </div>
-      <div class=" p-3">
         <a className="social-icon" href="https://twitter.com/hcoco1" aria-label='twitter' target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-x-twitter fa-2xl"></i>
-        </a>
-      </div>
-      <div class="p-3">
-        <a className="social-icon" href="https://codesandbox.io/u/hcoco1" aria-label='codesandbox' target="_blank" rel="noopener noreferrer">
-          <i className="fa-brands fa-codepen fa-2xl" style={{ color: 'black' }}></i>
+        <FontAwesomeIcon icon={faXTwitter} size="2xl" style={{color: "#000000",}} />
         </a>
       </div>
     </div>
