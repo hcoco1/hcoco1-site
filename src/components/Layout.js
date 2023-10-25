cd4055f68dbe59ab3b646a6cd98790c311db4457
Ivan <arias.ivan@gmail.com>
2023-10-25 11:02:06 -0400

    canonicalUrl fixed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ import Header from "./Header";
 import FooterMdb from "./FooterMdb";
 
 export default function Layout() {
-  const canonicalUrl = "https://hcoco1.blog/";
+  const canonicalUrl = "https://hcoco1.com/";
 
   return (
     <div className="container">
